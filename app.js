1235cc154b45cc3ac11b6b37cd48c145149b2c6d
Esteban Rodriguez <erodriguezg7@gmail.com>
2022-04-24 22:03:05 -0500

    Agregando el endopoint POST /v1/explorers

--- app.js
+++ app.js
@@ -18,6 +18,30 @@ app.get('/v1/explorers',(req, res)=>{
     res.status(200).json(explorers)
 })
 
+app.get('/v1/explorers/:id',(req, res)=>{
+
+    console.log(`GET Explorers V1 API ${new Date()}`)
+    const explorer1 = {id: 1, name: "ESteban"}
+    // HTTP CODE STATUS: 200 - success
+    res.status(200).json(explorer1)
+})
+
+// Metodo POST usado para insertar Datos
+app.post('/v1/explorers', (req, res)=>{
+
+    // Agregar aqui la logica de persistencia de Datos
+
+    console.log(`POST Explorers V1 API ${new Date()}`)
+
+    console.log(req.body)
+
+    res.status(201).json({result: "success", message: "Creado Existosamente"})
+
+
+});
+
+
+
 app.listen(port, ()=>{
 
     console.log(`Example app listening port ${port}`)
